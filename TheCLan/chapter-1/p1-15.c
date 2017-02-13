808e71286db7adc9001b1ed09fca88e27828bf35
mx-in <mouxin1993@gmail.com>
2017-02-14 00:22:34 +0800

    2.14 Exercise

---
+++ TheCLan/chapter-1/p1-15.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double fahrtocels(float fahr);
+
+int main(void)
+{
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower =  0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	while (fahr < upper) {
+		printf("%3.0f, %6.1f\n", fahr, fahrtocels(fahr));
+		fahr = fahr + step;
+	}
+}
+
+double fahrtocels(float fahr)
+{
+	double cels = (5.0/9.0) * (fahr-32.0);
+	return cels;
+}
