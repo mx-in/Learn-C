01fe2f334370b595e4e42fbf8d759fb323fd4c45
mx-in <mouxin1993@gmail.com>
2017-02-17 00:34:00 +0800

    1.10 External Variables and Scope

--- TheCLan/chapter-1/d1.10outervar.c
+++ TheCLan/chapter-1/d1.10outervar.c
@@ -28,19 +28,20 @@ int getlinen(void)
 	int i, c;
 	extern char line[];
 
-	for (i = 0; i < MAXLINE-1 && (c = getchar()) != EOF && i != '\n'; ++i)
+	for (i = 0; i < MAXLINE-1 && (c = getchar()) != EOF && c != '\n'; ++i)
 		line[i] = c;
 	if (c == '\n') {
 		line[i] = c;
 		++i;
 	}
-	line[i] = '\n';
+	line[i] = '\0';
 	return i;
 }
 
 void copy(void)
 {
 	int i;
+	extern char longest[], line[];
 
 	i = 0;
 	while ((longest[i] = line[i]) != '\0')
