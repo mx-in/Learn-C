2067436338a4a1388f300a580a2f20b37a300dd8
mx-in <mouxin1993@gmail.com>
2017-02-17 00:00:33 +0800

    1.10 outer vairable

---
+++ TheCLan/chapter-1/d1.10outervar.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define MAXLINE 1000 
+int max;
+char line[MAXLINE];
+char longest [MAXLINE]; 
+
+int getlinen(void);
+void copy(void);
+
+int main(void)
+{
+	int len;
+
+	max = 0;
+	while ((len = getlinen()) > 0)
+		if (len > max) {
+			max = len;	
+			copy();
+		}
+	printf("%s", longest );
+
+	return 0;
+}
+
+int getlinen(void)
+{
+	int i, c;
+	extern char line[];
+
+	for (i = 0; i < MAXLINE-1 && (c = getchar()) != EOF && i != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\n';
+	return i;
+}
+
+void copy(void)
+{
+	int i;
+
+	i = 0;
+	while ((longest[i] = line[i]) != '\0')
+		++i;
+}
